98984f01414d7d9e6493bef3e174cbd993327e98
rubiant26074 <haep0506@gmail.com>
2026-08-05 23:23:59 +0700

    security(route): revert path parameter for migrate run route

--- routes/web.php
+++ routes/web.php
@@ -1002,11 +1002,7 @@ Route::middleware(MmsAuthenticate::class)->group(function (): void {
 
 Route::get('/run-migration-secret-xyz', function () {
     try {
-        $params = ['--force' => true];
-        if (request()->has('path')) {
-            $params['--path'] = request()->input('path');
-        }
-        $exitCode = Illuminate\Support\Facades\Artisan::call('migrate', $params);
+        $exitCode = Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
         return "Migrasi berhasil dijalankan dengan kode status: " . $exitCode . "<br><pre>" . Illuminate\Support\Facades\Artisan::output() . "</pre>";
     } catch (\Exception $e) {
         return "Gagal menjalankan migrasi: " . $e->getMessage();
